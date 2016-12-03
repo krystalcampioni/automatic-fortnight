e474b0524dcfee92ac592ae2c3a7570ad23f21ca
Krystal Campioni <krystal.designer@gmail.com>
2016-12-03 16:20:22 -0200

    stylize product info section

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var carousel = new ch.Carousel(ch('.related-products__carousel')[0], {
-  pagination: true,
-  autoHeight: false,
+  pagination: false,
+  autoHeight: true,
   autoMargin: false,
-  limitPerPage: 2,
+  limitPerPage: 3,
 });
